1bbb943611b1d0ee7f5d03f356a7338158853c8b
kavin <kavin851018@gmail.com>
2018-08-11 15:04:09 +0800

    首頁建置完畢 準備加入價格拉桿

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,17 @@
     <title>評屋網</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <style>
+        /* Make the image fully responsive */
+        .carousel-inner img {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
 <body>
 
@@ -20,14 +28,114 @@
         <div class="col-sm-4">
             <h3>平均估價: $3700</h3>
             <img src="/image/1.jpg" class="img-thumbnail" alt="Cinque Terre">
+            <!-- Button to Open the Modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                查看圖片
+            </button>
+            <!-- Button to Open the Modal -->
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModa2">
+                點我評價
+            </button>
         </div>
         <div class="col-sm-4">
             <h3>平均估價: $3700</h3>
             <img src="/image/2.jpg" class="img-thumbnail" alt="Cinque Terre">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                查看圖片
+            </button>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModa2">
+                點我評價
+            </button>
         </div>
         <div class="col-sm-4">
             <h3>平均估價: $3700</h3>
             <img src="/image/3.jpg" class="img-thumbnail" alt="Cinque Terre">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                查看圖片
+            </button>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModa2">
+                點我評價
+            </button>
+        </div>
+    </div>
+</div>
+
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">平均估價: $3700</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div id="demo" class="carousel slide" data-ride="carousel">
+
+                    <!-- Indicators -->
+                    <ul class="carousel-indicators">
+                        <li data-target="#demo" data-slide-to="0" class="active"></li>
+                        <li data-target="#demo" data-slide-to="1"></li>
+                        <li data-target="#demo" data-slide-to="2"></li>
+                    </ul>
+
+                    <!-- The slideshow -->
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="/image/1.jpg" alt="Los Angeles" width="1100" height="500">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/image/2.jpg" alt="Chicago" width="1100" height="500">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/image/3.jpg" alt="New York" width="1100" height="500">
+                        </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+                </div>
+
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">關閉</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- The Modal2 -->
+<div class="modal" id="myModa2">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Modal Heading</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                Modal body..
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
         </div>
     </div>
 </div>
