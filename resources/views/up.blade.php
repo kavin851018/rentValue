ae9c4bc1aa58644c58958252c95fcaa8e6fb9103
kavin <kavin851018@gmail.com>
2018-08-13 10:31:48 +0800

    檔案上傳按鈕套bootstrap

--- resources/views/up.blade.php
+++ resources/views/up.blade.php
@@ -2,12 +2,22 @@
 <html>
 <body>
 
+@extends('master')
+
+
+@section('content')
 <form action="/user/upload" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="fileToUpload[]" id="fileToUpload" multiple="true">
-    <input type="submit" value="Upload Image" name="submit">
+
+    <div class="custom-file">
+        <input type="file" class="custom-file-input" name="fileToUpload[]" id="validatedCustomFile" multiple="true" required>
+        <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+        <div class="invalid-feedback">Example invalid custom file feedback</div>
+    </div>
+    <input type="submit" class="btn btn-primary" value="Upload Image" name="submit">
     {!! csrf_field() !!}
 </form>
+    @endsection
 
 </body>
 </html>
