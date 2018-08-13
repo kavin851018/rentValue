ddd089433a02df6f9450f286a84ecf0fec3d14b1
kavin <kavin851018@gmail.com>
2018-08-13 10:05:36 +0800

    成功上傳多個檔案並且存入資料夾

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,17 @@ class UserController extends Controller
 
     }
     public function uploadObject(Request $request){
-        $path = $request->file('fileToUpload')->store('image');
-        return $path;
+
+        if($request->hasFile('fileToUpload')){
+            foreach($request->fileToUpload as $file){
+                $filename = $file->getClientOriginalName();
+                $filesize = $file->getClientSize();
+                $file->storeAs('image2',$filename);
+                print_r($filename."<br>");
+            }
+        }
+        return 'yes';
+//        $path = $request->file('fileToUpload')->store('image');
+//        return $path;
     }
 }
--- resources/views/up.blade.php
+++ resources/views/up.blade.php
@@ -4,7 +4,7 @@
 
 <form action="/user/upload" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="file" name="fileToUpload[]" id="fileToUpload" multiple="true">
     <input type="submit" value="Upload Image" name="submit">
     {!! csrf_field() !!}
 </form>
