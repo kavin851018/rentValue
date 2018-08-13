4c1b90edb452f65e78bd851b114975fdd80556ce
kavin <kavin851018@gmail.com>
2018-08-13 14:50:18 +0800

    完成前端上傳圖片驗證、多檔

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,22 +1,26 @@
 @extends('master')
 @section('resource')
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/css/bootstrapValidator.min.css"/>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap/3.2.0/css/bootstrap.min.css"/>
+
+    <!-- Include FontAwesome CSS if you want to use feedback icons provided by FontAwesome -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/fontawesome/4.1.0/css/font-awesome.min.css" />
+
+    <!-- BootstrapValidator CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css"/>
+
+    <!-- jQuery and Bootstrap JS -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <!-- BootstrapValidator JS -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.bootstrapvalidator/0.5.0/js/bootstrapValidator.min.js"></script>
 @endsection
 @section('style')
 
 @endsection
 @section('script')
-    <script type="application/javascript">
-        $('#inputGroupFile02').change(function(e){
-            var fileNumber = e.target.files.length;
-            var fileName="";
-            for(var i=0;i<fileNumber;i++){
-                fileName+=e.target.files[i].name+",";
-            }
-            $('.custom-file-label').html(fileName);
-        });
-
+    <script>
         $(document).ready(function () {
             $('#test').bootstrapValidator({
                 live: 'enabled',
@@ -26,23 +30,31 @@
                     validating: 'glyphicon glyphicon-refresh'
                 },
                 fields: {
-                    fileupload: {
+                    'fileToUpload[]': {
                         validators: {
+                            notEmpty: {
+                                // enabled is true, by default
+                                message: '請選擇1或多個檔案'
+                            },
                             file: {
-                                extension: 'doc,docx,pdf,zip,rtf',
-                                type: 'application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/rtf,application/zip',
-                                maxSize: 5 * 1024 * 1024, // 5 MB
-                                message: 'The selected file is not valid, it should be (doc,docx,pdf,zip,rtf) and 5 MB at maximum.'
+                                extension: 'jpeg,png,jpg',
+                                type: 'image/jpeg,image/png,image/jpg',
+                                maxSize: 2048 * 1024,
+                                message: '請選擇正確格式! 如 jpg,png '
                             }
                         }
                     }
                 }
             });
+        });
 
-            $('#removeFile').on('click', function () {
-                document.getElementById('fileupload').value = "";
-                $('#test').bootstrapValidator('revalidateField', 'fileupload');
-            });
+        $('#fileupload').change(function(e){
+            var fileNumber = e.target.files.length;
+            var fileName="";
+            for(var i=0;i<fileNumber;i++){
+                fileName+=e.target.files[i].name+",";
+            }
+            $('.custom-file-label').html(fileName);
         });
     </script>
 @endsection
@@ -52,28 +64,22 @@
 @section('content')
     <div class="row">
         <div class="col-sm-8">
-            <form action="/user/upload" method="post" enctype="multipart/form-data">
-                <div class="input-group mb-3">
+            <form action="/user/upload" method="post" enctype="multipart/form-data" id="test">
+
+
+                <div class="form-group" >
+                    選擇要上傳的圖片:
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="inputGroupFile02" multiple="true"/>
-                        <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
-                    </div>
-                    <div class="input-group-append">
-                        <button class="btn btn-primary">Upload</button>
-                    </div>
-                </div>
-
-
-
-                <div class="form-group" id="test">
-                    <label for="resume" class="col-sm-2 control-label">Upload stuff</label>
-                    <div class="col-sm-8">
-                        <input type="file" name="fileupload" id="fileupload" multiple="true" />
-                    </div>
-                    <div class="col-sm-2">	<a class="btn btn-danger pull-right" id="removeFile">Remove</a>
+                        <input type="file" class="custom-file-input" name="fileToUpload[]" id="fileupload" multiple="true" required>
+                        <label class="custom-file-label" for="validatedCustomFile">選擇檔案...</label>
 
                     </div>
                 </div>
+
+
+
+
+                {!! csrf_field() !!}
 
 
 
@@ -86,9 +92,9 @@
                     <label for="comment">居住情況</label>
                     <textarea class="form-control" rows="5" id="comment"></textarea>
                 </div>
-
-                <input type="submit" class="btn btn-primary" value="送出" name="submit">
-                {!! csrf_field() !!}
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="送出" name="send">
+                </div>
 
             </form>
         </div>
