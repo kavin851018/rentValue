e21520b7078ad849294b6bff281d9436e0823a59
kavin <kavin851018@gmail.com>
2018-09-01 14:59:29 +0800

    放入一段要求位置的script，不過需要httos或localhost

--- resources/views/getIP.blade.php
+++ resources/views/getIP.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
     {{$ipaddress}}
+    <p>Click the button to get your coordinates.</p>
 
+    <button onclick="getLocation()">Try It</button>
 
+    <p id="demo"></p>
+
+    @endsection
+
+@section('script')
+    <script>
+        var x = document.getElementById("demo");
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+        function showPosition(position) {
+            x.innerHTML = "Latitude: " + position.coords.latitude +
+                "<br>Longitude: " + position.coords.longitude;
+        }
+    </script>
     @endsection
